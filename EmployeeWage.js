d388d3d9a98888a9b34fb496e478503c52a9a86a
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-08 21:55:17 -0800

    Refactored : Functionality to get No of Working days and Calculate Daily Employeewage is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -38,3 +38,29 @@
         console.log("Employee Wage: " + empWage);
     }
 }
+
+//UC-3 Refactor Code Write function to get Working Hours
+{
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HRS = 4;
+    const FULL_TIME_HRS = 8;
+    const WAGE_PER_HOUR = 20;
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HRS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HRS;
+                break;
+            default:
+                return 0;
+        }
+    }
+    let empHRS = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHRS = getWorkingHours(empCheck);
+    let empWage = empHRS * WAGE_PER_HOUR;
+    console.log("Employee hours :" + empHRS + " Employee Wage: " + empWage);
+}
+
