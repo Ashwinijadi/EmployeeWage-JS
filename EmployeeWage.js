21f54684c994c5618b4df3765e51e05deb3ec31b
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-08 22:06:18 -0800

    Refactored : Functionality to Calculate Employeewage till 160 working hours or 20 working days is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -91,3 +91,36 @@
     let empWage = empHRS * WAGE_PER_HOUR;
     console.log("Total Hrs: " + empHRS + "   Employee Wage: " + empWage);
 }
+
+//UC-5 Calculate Wage Till 160Hrs or 20Days
+{
+    const MAX_HRS_IN_MONTH = 100;
+    const NO_OF_WORKING_DAYS = 20;
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HRS = 4;
+    const FULL_TIME_HRS = 8;
+    const WAGE_PER_HOUR = 20;
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                console.log("Part Time employee is present");
+                return PART_TIME_HRS;
+            case IS_FULL_TIME:
+                console.log("Part Time employee is present");
+                return FULL_TIME_HRS;
+            default:
+                console.log("employee is absent");
+                return 0;
+        }
+    }
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays<  NO_OF_WORKING_DAYS ) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("TotalWorkingDays are : " + totalWorkingDays + "  Total Hrs: " + totalEmpHrs + "   Employee Wage: " + empWage);
+}
