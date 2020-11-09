636d4acc236ad714a98744eac0e3429dc74eb6e3
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-08 22:01:28 -0800

    Refactored : Functionality to Calculate Employeewage for month assuming 20 working days is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-//WELcome
+//WELCOME message
 {
     console.log("Welcome to Employee Wage Programming")
 }
@@ -64,3 +64,30 @@
     console.log("Employee hours :" + empHRS + " Employee Wage: " + empWage);
 }
 
+
+//UC-4 calculate wage for month assuming 20 Working days 
+{
+    const NO_OF_WORKING_DAYS = 20;
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HRS = 4;
+    const FULL_TIME_HRS = 8;
+    const WAGE_PER_HOUR = 20;
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HRS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HRS;
+            default:
+                return 0;
+        }
+    }
+    let empHRS = 0;
+    for (let day = 0; day < NO_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        empHRS += getWorkingHours(empCheck);
+    }
+    let empWage = empHRS * WAGE_PER_HOUR;
+    console.log("Total Hrs: " + empHRS + "   Employee Wage: " + empWage);
+}
