f2393753163b2f5487d8e7c5167a13910fe675d1
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-10 18:05:32 -0800

    added :functionality of calculating Total Wage and total hours are added and printing of Fullworking days,PartWorking days and Non Working days are added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,6 +2,14 @@
 {
     console.log("Welcome to Employee Wage Programming")
 }
+//constants
+const MAX_HRS_IN_MONTH = 100;
+const NO_OF_WORKING_DAYS = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HOUR = 20;
 //UC 1  Check Employee present or absent
 {
     const IS_ABSENT = 0
@@ -16,11 +24,6 @@
 
 // UC-2 Calculate EmployeeWage based on PART-TIME or FULL-TIME
 {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HRS = 4;
-    const FULL_TIME_HRS = 8;
-    const WAGE_PER_HOUR = 20;
     {
         let empHRS = 0;
         let empCheck = Math.floor(Math.random() * 10) % 3;
@@ -41,11 +44,6 @@
 
 //UC-3 Refactor Code Write function to get Working Hours
 {
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HRS = 4;
-    const FULL_TIME_HRS = 8;
-    const WAGE_PER_HOUR = 20;
     function getWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
@@ -67,12 +65,7 @@
 
 //UC-4 calculate wage for month assuming 20 Working days 
 {
-    const NO_OF_WORKING_DAYS = 20;
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HRS = 4;
-    const FULL_TIME_HRS = 8;
-    const WAGE_PER_HOUR = 20;
+
     function getWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
@@ -94,13 +87,6 @@
 
 //UC-5 Calculate Wage Till 160Hrs or 20Days
 {
-    const MAX_HRS_IN_MONTH = 100;
-    const NO_OF_WORKING_DAYS = 20;
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HRS = 4;
-    const FULL_TIME_HRS = 8;
-    const WAGE_PER_HOUR = 20;
     function getWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
@@ -127,12 +113,10 @@
 function calcDailyWage(empHrs) {
     return empHrs * WAGE_PER_HOUR;
 }
-const WAGE_PER_HOUR = 20;
-const MAX_HRS_IN_MONTH = 160;
-const NO_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
+let empDailyHrsMap = new Map();
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
@@ -207,8 +191,6 @@ console.log("UC 7D -first time fulltime wage was earned on day " + mapDayWithWag
 // UC 8-Store Day and DailyWage using Map and calculate totalWage using map
 //Uc6 Arrays and UC7 Maps
 {
-    const MAX_HRS_IN_MONTH = 160;
-    const NUM_OF_WORKING_DAYS = 20;
     let totalEmpHrs = 0;
     let totalWorkingDays = 0;
     let empDailyWageArr = new Array();
@@ -218,17 +200,52 @@ console.log("UC 7D -first time fulltime wage was earned on day " + mapDayWithWag
         return empHrs * WAGE_PER_HOUR;
     }
 
-    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
         totalWorkingDays++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
         let empHrs = getWorkingHours(empCheck);
         totalEmpHrs += empHrs;
         empDailyWageArr.push(calcDailyWage(empHrs));
+        empDailyHrsMap.set(totalWorkingDays, empHrs);
         empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
     }
     console.log(empDailyWageMap);
+    console.log("employee hours map", empDailyHrsMap);
     function totalWages(totalWage, dailyWage) {
         return totalWage + dailyWage;
     }
-    console.log("UC7A-Employee Wage Map TotalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+    console.log("UC8-Employee Wage Map Totalwage : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
 }
+
+//UC-9 Calculate total hrs,total working days and print Full,part and No Working Days using Arrow Functions
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsMap.set(totalWorkingDays, empHrs);
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+console.log("UC9A - Employee wage with arrow : " + "total Hours : " + totalHours + "total employee Wage : " + totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working days are : ", fullWorkingDays);
+console.log("Part Working days are : ", partWorkingDays);
+console.log("No Working days are : ", nonWorkingDays);
+
+
+
+
+
+
