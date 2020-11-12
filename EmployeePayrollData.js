b7dd55eb5c8d98d84a129a60ce6259814755c95f
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-12 09:43:59 -0800

    added : Functionality of Regex is used to check whether information is following pattern if not throw error

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,47 +2,42 @@
 //UC 11 to create Employee Payroll Data with id, name and salary
 //UC 12 Extend EmployeePayroll Data to store gender,date
 
+const nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+const genderRegex = RegExp('^[m,M,f,F]{1}$');
 class EmployeePayrollData {
     id;
     salary;
     gender;
     startDate;
     //constructor
-    constructor(id, name, salary, gender, startDate) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-        this.gender = gender;
-        this.startDate = startDate;
+    constructor(...parameter) {
+        if (!parameter[0] > 0) throw "Id is Incorrect"
+        this.id = parameter[0];
+        if (!nameRegex.test(parameter[1])) throw "name is Incorrect !!!"
+        this.name = parameter[1];
+        if (!parameter[2] > 0) throw "Salary is Incorrect !!!"
+        this.salary = parameter[2];
+        if (!genderRegex.test(parameter[3])) throw "gender is Incorrect !!!"
+        this.gender = parameter[3];
+        if (!parameter[4] > new Date()) throw "Date is Incorrect !!!"
+        this.startDate = parameter[4];
     }
 
-    get name() { return this._name; }
-    set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if (nameRegex.test(name)) 
-            this._name = name;
-        else
-            throw 'Name is Incorrect!!';
-    }
     toString() {
         const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-        const empDate = this.startDate === undefined ? "undefined" :
-            this.startDate.toLocaleDateString("en-US", options);
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
         return "id = " + this.id + " , name : " + this.name + " , salary : " + this.salary + "," +
-            "gender=" + this.gender + " , startDate=" + empDate;
+            "gender = " + this.gender + " date = " + empDate;
     }
 }
 
-let employeePayrollData = new EmployeePayrollData(1, "ashu", 30000);
-console.log(employeePayrollData.toString());
-employeePayrollData.id = 2;
-employeePayrollData.salary = 20000;
 try {
-    employeePayrollData.name = "john";
-    console.log(employeePayrollData.toString());
+    let newEmployeePayrollData = new EmployeePayrollData(1, "Asritha", 1000, "F", new Date());
+    console.log(newEmployeePayrollData.toString());
 } catch (e) {
     console.error(e);
 }
-let newEmployeePayrollData = new EmployeePayrollData(1, "asritha", 30000, "F", new Date());
-console.log(newEmployeePayrollData.toString());
+
+
+
 
